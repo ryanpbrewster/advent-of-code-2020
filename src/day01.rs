77a0e7c2ec611e4f1062389614eaaa1d815b3951
Ryan P. Brewster <RyanPBrewster@gmail.com>
2020-12-01 17:03:03 -0500

    elide

--- src/day01.rs
+++ src/day01.rs
@@ -1,15 +1,6 @@
-use itertools::iproduct;
-
-fn find_pair(input: &[i32], target: i32) -> Option<(&i32, &i32)> {
-    iproduct!(input, input).find(|(&a, &b)| a + b == target)
-}
-fn find_triple(input: &[i32], target: i32) -> Option<(&i32, &i32, &i32)> {
-    iproduct!(input, input, input).find(|(&a, &b, &c)| a + b + c == target)
-}
-
 #[cfg(test)]
 mod test {
-    use super::{find_pair, find_triple};
+    use itertools::iproduct;
 
     #[test]
     fn small1() {
@@ -26,7 +17,9 @@ mod test {
             .map(|token| token.parse::<i32>().unwrap())
             .collect();
 
-        let (a, b) = find_pair(&entries, 2020).unwrap();
+        let (a, b) = iproduct!(&entries, &entries)
+            .find(|(&a, &b)| a + b == 2020)
+            .unwrap();
         assert_eq!(a * b, 514579);
     }
 
@@ -37,7 +30,9 @@ mod test {
             .split_ascii_whitespace()
             .map(|token| token.parse::<i32>().unwrap())
             .collect();
-        let (a, b) = find_pair(&entries, 2020).unwrap();
+        let (a, b) = iproduct!(&entries, &entries)
+            .find(|(&a, &b)| a + b == 2020)
+            .unwrap();
         assert_eq!(a * b, 996075);
     }
 
@@ -48,7 +43,9 @@ mod test {
             .split_ascii_whitespace()
             .map(|token| token.parse::<i32>().unwrap())
             .collect();
-        let (a, b, c) = find_triple(&entries, 2020).unwrap();
+        let (a, b, c) = iproduct!(&entries, &entries, &entries)
+            .find(|(&a, &b, &c)| a + b + c == 2020)
+            .unwrap();
         assert_eq!(a * b * c, 51810360);
     }
 }
