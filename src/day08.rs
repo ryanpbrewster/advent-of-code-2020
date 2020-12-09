990543c004f2549e71ae3ba421f1f62f4f7f485d
Ryan P. Brewster <RyanPBrewster@gmail.com>
2020-12-09 16:57:25 -0500

    implement interpreter

--- src/day08.rs
+++ src/day08.rs
@@ -1,3 +1,5 @@
+use std::collections::HashSet;
+
 use nom::{
     branch::alt,
     bytes::complete::{tag, take_while},
@@ -69,10 +71,50 @@ fn arg_parser(input: &str) -> IResult<&str, i32> {
     )(input)
 }
 
+#[derive(Debug)]
+struct Interpreter {
+    program: Vec<Instruction>,
+    acc: i32,
+    pos: i32,
+}
+impl Interpreter {
+    fn new(program: Vec<Instruction>) -> Interpreter {
+        Interpreter {
+            program,
+            acc: 0,
+            pos: 0,
+        }
+    }
+    fn step(&mut self) {
+        let instruction = &self.program[self.pos as usize];
+        match instruction.opcode {
+            OpCode::Jump => {
+                self.pos += instruction.arg;
+            }
+            OpCode::Nop => {
+                self.pos += 1;
+            }
+            OpCode::Acc => {
+                self.acc += instruction.arg;
+                self.pos += 1;
+            }
+        }
+    }
+}
+
+fn final_acc(mut interpreter: Interpreter) -> Option<i32> {
+    let mut visited = HashSet::new();
+    loop {
+        if !visited.insert(interpreter.pos) {
+            return Some(interpreter.acc);
+        }
+        interpreter.step();
+    }
+}
+
 #[cfg(test)]
 mod test {
-    use super::{instruction_parser, parse_instructions, Instruction};
-
+    use super::{final_acc, instruction_parser, parse_instructions, Instruction, Interpreter};
     #[test]
     fn parser_one() {
         assert_eq!(instruction_parser("nop +0").unwrap().1, Instruction::nop(0));
@@ -82,9 +124,8 @@ mod test {
             Instruction::jmp(-4)
         );
     }
-    #[test]
-    fn parser_list() {
-        const INPUT: &str = r"
+
+    const SMALL: &str = r"
             nop +0
             acc +1
             jmp +4
@@ -95,10 +136,24 @@ mod test {
             jmp -4
             acc +6
         ";
-
-        let parsed = parse_instructions(INPUT).unwrap();
+    #[test]
+    fn parser_list() {
+        let parsed = parse_instructions(SMALL).unwrap();
         assert_eq!(parsed[0], Instruction::nop(0));
         assert_eq!(parsed[1], Instruction::acc(1));
         assert_eq!(parsed[2], Instruction::jmp(4));
+    }
+
+    #[test]
+    fn small1() {
+        let interpreter = Interpreter::new(parse_instructions(SMALL).unwrap());
+        assert_eq!(final_acc(interpreter).unwrap(), 5);
+    }
+
+    #[test]
+    fn normal1() {
+        let raw = std::fs::read_to_string("data/day08.input").unwrap();
+        let interpreter = Interpreter::new(parse_instructions(&raw).unwrap());
+        assert_eq!(final_acc(interpreter).unwrap(), 1087);
     }
 }
