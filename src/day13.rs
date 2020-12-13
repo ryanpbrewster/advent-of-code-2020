b52921afd5c579e02f733e75d9d1732744f9dde4
Ryan P. Brewster <RyanPBrewster@gmail.com>
2020-12-13 17:46:05 -0500

    day 13 part 2

--- src/day13.rs
+++ src/day13.rs
@@ -1,20 +1,56 @@
+use itertools::Itertools;
 struct Schedule {
-    t0: i32,
-    ts: Vec<i32>,
+    lo: i64,
+    ts: Vec<Option<i64>>,
 }
 
-fn solve1(input: Schedule) -> i32 {
+fn solve1(input: Schedule) -> i64 {
     let (id, t) = input
         .ts
         .iter()
-        .map(|&id| (id, id - (input.t0 % id)))
+        .filter_map(|id| id.as_ref())
+        .map(|&id| (id, id - (input.lo % id)))
         .min_by_key(|&(_id, t)| t)
         .unwrap();
     id * t
 }
+
+fn solve2(ts: Vec<Option<i64>>) -> i64 {
+    for (&t1, &t2) in ts.iter().filter_map(|t| t.as_ref()).tuple_combinations() {
+        assert_eq!(gcd(t1 as i64, t2 as i64), 1);
+    }
+    // We need t such that:
+    //   t === 0 (mod ts[0])
+    //   t === -1 (mod ts[1])
+    //   t === -2 (mod ts[2])
+    let mut eqns: Vec<(i64, i64)> = ts
+        .into_iter()
+        .enumerate()
+        .filter_map(|(idx, t)| Some((idx as i64, t? as i64)))
+        .collect();
+    eqns.sort_by_key(|(_idx, t)| -t);
+
+    let mut x = 0;
+    let mut dx = 1;
+    for (idx, t) in eqns {
+        // looking for x + k * dx === -i (mod t)
+        let k = (0..).find(|k| (x + k * dx + idx) % t == 0).unwrap();
+        x = x + k * dx;
+        dx *= t;
+    }
+    x
+}
+
+fn gcd(m: i64, n: i64) -> i64 {
+    if n == 0 {
+        m
+    } else {
+        gcd(n, m % n)
+    }
+}
 #[cfg(test)]
 mod test {
-    use super::{solve1, Schedule};
+    use super::{gcd, solve1, solve2, Schedule};
 
     const SMALL: &str = r"
         939
@@ -22,15 +58,14 @@ mod test {
     ";
     fn parse(input: &str) -> Schedule {
         let mut lines = input.trim().lines().map(|l| l.trim());
-        let t0 = lines.next().unwrap().parse().unwrap();
+        let lo = lines.next().unwrap().parse().unwrap();
         let ts = lines
             .next()
             .unwrap()
             .split(',')
-            .filter(|&t| t != "x")
-            .map(|t| t.parse().unwrap())
+            .map(|t| t.parse().ok())
             .collect();
-        Schedule { t0, ts }
+        Schedule { lo, ts }
     }
     #[test]
     fn small1() {
@@ -43,5 +78,29 @@ mod test {
         let raw = std::fs::read_to_string("data/day13.input").unwrap();
         let input = parse(&raw);
         assert_eq!(solve1(input), 153);
+    }
+
+    #[test]
+    fn small2() {
+        assert_eq!(solve2(parse(SMALL).ts), 1068781);
+        assert_eq!(solve2(parse("0\n17,x,13,19").ts), 3417);
+        assert_eq!(solve2(parse("0\n67,7,59,61").ts), 754018);
+        assert_eq!(solve2(parse("0\n67,x,7,59,61").ts), 779210);
+        assert_eq!(solve2(parse("0\n67,7,x,59,61").ts), 1261476);
+        assert_eq!(solve2(parse("0\n1789,37,47,1889").ts), 1202161486);
+    }
+
+    #[test]
+    fn gcd1() {
+        assert_eq!(gcd(3, 9), 3);
+        assert_eq!(gcd(4, 10), 2);
+        assert_eq!(gcd(13, 19), 1);
+    }
+
+    #[test]
+    fn normal2() {
+        let raw = std::fs::read_to_string("data/day13.input").unwrap();
+        let input = parse(&raw);
+        assert_eq!(solve2(input.ts), 471793476184394);
     }
 }
